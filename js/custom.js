bcb6b55caeb7ef9f06112ad42c4bb426d8beba35
Juanma Romalde <juanma0089@hotmail.com>
2022-11-28 15:30:54 +0100

    Modificación formulario + modal en intereses

--- js/custom.js
+++ js/custom.js
@@ -20,9 +20,32 @@ function SlideRight() {
       document.getElementById("press").style.color = "#000";
     }
   }
-
+//* necesario para popovers de bootstrap
   const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
   const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+//* necesario para alert bootstrap
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-danger mt-3 alert-dismissible" role="alert">`,
+    `<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('El formulario no está activo actualmente')
+  })
+}
 
   //! evento al cargar
 
@@ -48,20 +71,20 @@ function SlideRight() {
 
   //! evento al hacer scroll y el elemento esté visible
 
-  // esta funcion comprueba si un elemento esta visible en pantalla
+  // esta función comprueba si un elemento esta visible en pantalla
   function isVisible(elm) {
     let rect = elm.getBoundingClientRect();
     let viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight)/1.3;
     return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
   }
   
-  // cuando se carga la página...
+  // cuando se carga la página
   window.addEventListener('DOMContentLoaded', (ev0) => {
-          // asignamos un evento scroll...
+  // asignamos un evento scroll
     window.addEventListener('scroll', (ev1) => {
-                  // y a todos los elementos con la clase paused...
+  // y a todos los elementos con la clase paused
       document.querySelectorAll(".paused").forEach(elm => {
-        if (isVisible(elm)){ // que sean visibles...
+        if (isVisible(elm)){ // que sean visibles
           elm.classList.remove("paused"); // les quitamos la clase paused
         }
       })
